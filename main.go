f66f65b742ccc65902a5307836b2c63cf1da23be
remy <remysalem@hotmail.fr>
2023-10-10 19:10:12 +0200

    Better exemple with handle of ctrl + c

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 
 	"github.com/Eznopot/tcp/tcp_client"
 	"github.com/Eznopot/tcp/tcp_server"
@@ -12,28 +14,29 @@ import (
 
 func server(port string) {
 	fmt.Println("server")
-	var chanMsg chan string
+	var chanMsg chan []byte
 	var wg *sync.WaitGroup
 
-	// create new channel of type string
-	chanMsg = make(chan string)
+	// close server on ctrl C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		tcp_server.Close()
+	}()
 
 	//start server
-	wg, _ = tcp_server.Server(port, func(client net.Conn, msg []byte) {
+	wg, chanMsg = tcp_server.Server(port, func(client net.Conn, msg []byte) {
 		fmt.Printf("Message from client: %s \n", string(msg))
 
-		chanMsg <- "Hello to all from server with chan"                  // send msg to all via channel
+		chanMsg <- []byte("Hello to all from server with chan")          // send msg to all via channel
 		tcp_server.SendAll([]byte("Hello to all from server with func")) // send msg to all via function
 
 		//send msg to client
 		tcp_server.Send(client, []byte("Hello from server to you")) // send msg to client via function
 	})
 
-	// loop to send messages from the channel
-	for msg := range chanMsg {
-		tcp_server.SendAll([]byte(msg))
-	}
-
+	//wait for the process to finish
 	wg.Wait()
 }
 
@@ -41,6 +44,15 @@ func client(port string) {
 	fmt.Println("client")
 
 	//connect to server
+
+	// close client on ctrl C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		tcp_client.Close()
+	}()
+
 	wg, chanMsg := tcp_client.Client(port, func(msg []byte) {
 		fmt.Printf("Message from server: %s \n", string(msg))
 	})
@@ -48,6 +60,8 @@ func client(port string) {
 	//same thing
 	chanMsg <- []byte("Hello from client via chan")       // send msg via channel
 	tcp_client.Send([]byte("Hello from client via func")) // send msg via function
+
+	//wait for the process to finish
 	wg.Wait()
 }
 
